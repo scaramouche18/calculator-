a7031d8339fcb42b8e1a0313bc6f5549003a908a
Adarsh <137529020+scaramouche18@users.noreply.github.com>
2023-09-01 10:33:00 +0530

    Add files via upload

---
+++ Firstclass.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Firstclass {
+ /**
+ * @param args
+ */
+public static void main(String[] args){
+ for (int counter = 0  ; counter < 5 ; counter++){
+  System.out.println(counter);
+ }
+ }
+ }
+
+
